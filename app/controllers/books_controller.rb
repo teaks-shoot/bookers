93b27e8e7dc35bc72bba72539386706a11b3129d
EC2 Default User <ec2-user@ip-172-31-12-200.ap-northeast-1.compute.internal>
2024-07-24 20:29:50 +0000

    [Add]createのflash_success

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,10 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    end
   end
 
   def show
